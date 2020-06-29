4f9a74a2439435d71f675eebfc111f129a3631bf
akram_home <akram.hamdiha@gmail.com>
2020-06-29 13:14:34 +0200

    add script display rectangle

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # test_video_run_run
+
+# to excute script into console unix, enter "  ruby rectangle.rb [widht] [length]
+
+
---
+++ rectangle.rb
@@ -0,0 +1,71 @@
+##################################################################################
+#########  Test Technique Lundi 29 Juin 2020 ------By----- Video Run Run-----#####
+#########--------------------------------------------------------------------#####
+#########  Script in Ruby Language-------------------------------------------#####
+#########  Display Rectangle with parameters widht an lenght-----------------#####
+#########--------------------------------------------------------------------#####
+#########  Author:-----Akram HAMDI-------------------------------------------#####
+#########  Contact: akram.hamdiHa@gmail.com----------------------------------#####
+#########--------------------------------------------------------------------#####
+##################################################################################
+
+
+
+
+if ARGV.size == 2
+    
+    width = ARGV[0] 
+    length = ARGV[1]
+
+
+    length = length.to_i
+    width = width.to_i
+
+    #display up cote
+    print "A"
+    in_widht = width -2
+    in_widht.times do
+        print "*"
+    end
+    if width > 1
+        print "A"
+    end
+
+    for x in 0..length-3 do
+        y=0
+        print "\n"
+
+        y += 1
+        # disply left cote
+        print "*"
+        
+        #display inside space
+        for y in y..width-2 do
+            print " "
+        end
+        # disply right cote
+        if width > 1
+            print "*"
+        end
+    end
+    print "\n"
+
+    #display down cote
+   
+    if length > 1
+        print "A"
+        x = 1
+        for x in 1..width - 2 do
+            x += 1
+            print "*"
+        end
+        if width > 1
+            print "A"
+        end
+    end    
+
+    print "\n"
+
+else
+    puts "Error!! Put 2 parametres: length, width, please"
+end
